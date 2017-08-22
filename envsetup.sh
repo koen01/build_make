76f4e890008830038ea5f55f118d0ea6f11c3c5e
Ricardo Cerqueira <github@cerqueira.org>
2017-08-22 23:06:24 +0200

    Preliminary support for product builds
    Change-Id: I2a5e70dda973a6fcdbba0d5e26b35b99d3f1aea2

--- envsetup.sh
+++ envsetup.sh
@@ -144,6 +144,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^scorpion_") ; then
+       SCORPION_BUILD=$(echo -n $1 | sed -e 's/^scorpion_//g')
+    else
+       SCORPION_BUILD=
+    fi
+    export SCORPION_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
